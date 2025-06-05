bd5d17ff26830e7631a28bf56d5ee08bed22c222
Jason Zimdars <jz@37signals.com>
2025-06-05 16:56:26 -0500

    Clear `indexed_by`, too

--- app/controllers/concerns/filter_scoped.rb
+++ app/controllers/concerns/filter_scoped.rb
@@ -6,7 +6,7 @@ module FilterScoped
   end
 
   private
-    DEFAULT_PARAMS = { indexed_by: "newest" }
+    DEFAULT_PARAMS = { indexed_by: "latest" }
 
     def set_filter
       @filter = Current.user.filters.from_params params.reverse_merge(**DEFAULT_PARAMS).permit(*Filter::PERMITTED_PARAMS)
